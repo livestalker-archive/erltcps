a125ba70ca99574f5503e6eb061695c79e126e9d
LiveStalker <alexey@livestalker.net>
2010-10-05 22:49:58 +0400

    new file:   ebin/erltcps.app 	new file:   src/tcp_fsm.erl 	modified:   src/tcp_fsm.erl

---
+++ src/tcp_fsm.erl
@@ -0,0 +1,14 @@
+%%%-------------------------------------------------------------------
+%%% @author Alexey Grebenshchikov alexey@livestalker.net
+%%% @copyright (C) 2010, Alexey Grebenshchikov
+%%% @doc
+%%% TCP FSM
+%%% @end
+%%% Created :  5 Oct 2010 by Alexey Grebenshchikov
+%%%-------------------------------------------------------------------
+-module(tcp_fsm).
+
+-compile(export_all).
+
+test() ->
+	1+1.
