5abfb5e64c1e691d42452cc862417c01f6d120a3
LiveStalker <alexey@livestalker.net>
2011-06-22 09:48:34 +0400

    new file:   .gitignore 	modified:   README 	modified:   src/erltcps.erl 	modified:   src/tcp_client_sup.erl 	modified:   src/tcp_fsm.erl 	modified:   src/tcp_listener.erl 	modified:   src/tcp_server_sup.erl

--- src/tcp_listener.erl
+++ src/tcp_listener.erl
@@ -1,24 +1,8 @@
-%%%-------------------------------------------------------------------
-%%% @author LiveStalker alexey@livestalker.net
-%%% @copyright (C) 2010, LiveStalker
-%%% @doc
-%%% TCP listener
-%%% @end
-%%% Created :  5 Oct 2010 by LiveStalker
-%%%-------------------------------------------------------------------
 -module(tcp_listener).
--author('alexey@livestalker.net').
-
 -behaviour(gen_server).
-
-%% API
 -export([start_link/1]).
-
-%% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 		 terminate/2, code_change/3]).
-
-%%Internal API
 -export([accept_func/1]).
 
 -define(SERVER, ?MODULE). 
@@ -29,44 +13,8 @@
 		  module          %% FSM handling module
 		 }).
 
-%%%===================================================================
-%%% API
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @spec start_link(Port::integer()) -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
-
 start_link(Port) ->
 	gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Accept connection
-%%
-%% @spec accept() -> ok
-%% @end
-%%--------------------------------------------------------------------
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
 
 init([Port]) ->
 	Options = [{packet, raw}, {active, once}, {reuseaddr, true}],
@@ -80,83 +28,26 @@ init([Port]) ->
 			{stop, Reason}
    end.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 
 handle_call(_Request, _From, State) ->
 	Reply = ok,
 	{reply, Reply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 
 handle_cast(_Msg, State) ->
 	{noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
+
 handle_info(_Info, State) ->
 	{noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
 
 terminate(_Reason, #state{listener = LSocket} = _State) ->
 	gen_tcp:close(LSocket),
 	ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
-
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
 
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
 accept_func(LSocket) ->
 	{ok, Socket} = gen_tcp:accept(LSocket),
 	error_logger:info_msg("Accept connection: ~p.\n", [Socket]),
--- src/tcp_server_sup.erl
+++ src/tcp_server_sup.erl
@@ -1,62 +1,18 @@
-%%%-------------------------------------------------------------------
-%%% @author LiveStalker alexey@livestalker.net
-%%% @copyright (C) 2010, LiveStalker
-%%% @doc
-%%% TCP server top supervisor
-%%% @end
-%%% Created :  5 Oct 2010 by LiveStalker
-%%%-------------------------------------------------------------------
 -module(tcp_server_sup).
--author('alexey@livestalker.net').
-
 -behaviour(supervisor).
-
-%% API
 -export([start_link/1]).
-
-%% Supervisor callbacks
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 -define(MAX_RESTART, 5). %% Max restarts
 -define(MAX_TIME, 60).   %% Seconds between restarts
 
-%%%===================================================================
-%%% API functions
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the supervisor
-%%
-%% @spec start_link(Port::integer()) -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
 start_link(Port) ->
 	supervisor:start_link({local, ?SERVER}, ?MODULE, [Port]).
 
-%%%===================================================================
-%%% Supervisor callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Whenever a supervisor is started using supervisor:start_link/[2,3],
-%% this function is called by the new process to find out about
-%% restart strategy, maximum restart frequency and child
-%% specifications.
-%%
-%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
-%%                     ignore |
-%%                     {error, Reason}
-%% @end
-%%--------------------------------------------------------------------
 init([Port]) ->
-	%% supervisor flags
 	Flags = {one_for_one, ?MAX_RESTART, ?MAX_TIME},
 	
-	%% specification of child processes
 	Spec = [
 			{tcp_listener,                       %% Id
 			 {tcp_listener, start_link, [Port]}, %% StartFunc = {M, F, A}
@@ -73,7 +29,3 @@ init([Port]) ->
 			[tcp_client_sup]}],
 	
 	{ok, {Flags, Spec}}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
